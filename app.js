b1dc7489ac5bbd225d6dcfd3e3b74e0fc222bf0a
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-09-19 21:02:31 +0000

    add config for connection to sql database

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
+const sequelize = require('./config/sequelize');
 
 const app = express();
 const port = 3000;
+
+app.use(morgan('dev'));
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('connection to db successful');
+  })
+  .catch((err) => {
+    console.log('error connecting to db', err);
+  });
 
 app.get('/', (req, res) => {
   res.json('hello there');
