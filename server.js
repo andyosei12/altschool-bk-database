254d195b128fb7e8cd0d8261992491b375aa3fdb
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-09-28 08:13:48 +0000

    add config for mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const app = express();
+const db = require('./db');
 
+const app = express();
 const port = 5000;
+
+db.connect();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
