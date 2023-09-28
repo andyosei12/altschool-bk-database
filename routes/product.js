7f59b946734c8f8edc2a4c15557c2f3275b62427
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-09-28 06:49:32 +0000

    remove unused import

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const verifyUser = require('../middlewares/user/verifyUser');
 const { getProducts } = require('../controllers/product');
 
